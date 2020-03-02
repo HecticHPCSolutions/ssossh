1d0fa9e7a241ad5333717188cbe2a2aa43db7170
Chris Hines <chris.hines@monash.edu>
2020-03-02 14:50:30 +1100

    add setup, add comments, refactor function names

--- ssossh/__main__.py
+++ ssossh/__main__.py
@@ -1,18 +1,25 @@
 import json
 import webbrowser
 import os
-from functools import partial
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from jinja2 import Template
 import queue
 import subprocess
 import requests
 import pathlib
+from functools import partial
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from jinja2 import Template
 
-q=queue.Queue()
+q = queue.Queue()
+
 
 class MyRequestHandler(BaseHTTPRequestHandler):
-    def __init__(self, port, logout, *args,**kwargs):
+    """
+    The request handler runs twice. Once serves a snippet of
+    javascript which takes the token in the URL framgment and puts in into
+    a request parameter.
+    The second run receives the token as a request parameter and puts it in the queue
+    """
+    def __init__(self, port, logout, *args, **kwargs):
         self.port = port
         self.logout = logout
         super(MyRequestHandler, self).__init__(*args, **kwargs)
@@ -21,27 +28,33 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-    
+
     def do_GET(self, *args, **kwargs):
         if "callback" in self.path:
             self._set_headers()
             currpath = pathlib.Path(__file__).parent.absolute()
-            with open(os.path.join(currpath,'templates','call_back.html'),'rb') as f:
+            with open(os.path.join(currpath, 'templates', 'call_back.html'),
+                      'rb') as f:
                 tsrc = f.read()
                 t = Template(tsrc.decode())
-                self.wfile.write(t.render(port=self.port,logout=self.logout).encode())
+                self.wfile.write(t.render(port=self.port,
+                                          logout=self.logout).encode())
             return
         else:
             q.put(self.path)
 
+
 def make_key():
+    """
+    Generate a keyfile (using ssh-keygen)
+    """
     keypath = os.path.expanduser('~/.ssh/ssossh-key')
     try:
         mode = os.stat(keypath).st_mode
         import stat
         if stat.S_ISREG(mode):
             try:
-                rm_cert(keypath)
+                rm_key_agent(keypath)
             except subprocess.CalledProcessError:
                 pass
             try:
@@ -58,42 +71,68 @@ def make_key():
                 pass
     except FileNotFoundError:
         pass
-        
-    subprocess.call(['ssh-keygen','-N','','-f',keypath],stdout=subprocess.DEVNULL,stderr=subprocess.DEVNULL)
+    subprocess.call(['ssh-keygen', '-N', '', '-f', keypath],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
     return keypath
 
-def sign_cert(keypath,token,url):
-    with open(keypath+'.pub','r') as f:
+
+def sign_cert(keypath, token, url):
+    """
+    give a public key and a OAuth2 token,
+    use the token to access the signing endpoint and save
+    the resulting certificate
+    """
+    with open(keypath + '.pub', 'r') as f:
         pub_key = f.read()
     sess = requests.Session()
-    headers = {"Authorization":"Bearer %s"%token}
-    data = {"public_key":pub_key}
+    headers = {"Authorization": "Bearer {}".format(token)}
+    data = {"public_key": pub_key}
     resp = sess.post(url, json=data, headers=headers, verify=False)
     data = resp.json()
     cert = data['certificate']
-    with open(keypath+"-cert.pub",'w') as f:
+    with open(keypath + "-cert.pub", 'w') as f:
         f.write(cert)
 
-def start_agent():
-    pass
 
-def rm_cert(keypath):
-    subprocess.call(['ssh-add','-d',keypath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+def rm_key_agent(keypath):
+    """
+    Remove the key/cert from the agent
+    """
+    subprocess.call(['ssh-add', '-d', keypath],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
 
-def add_cert(keypath):
-    subprocess.call(['ssh-add',keypath], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def add_key_agent(keypath):
+    """
+    Add the key and cert to the agent
+    """
+    subprocess.call(['ssh-add', keypath],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
+
 
 def do_request(authservice):
+    """
+    Open a web browser window
+    and request an OAuth2 token to sign certificates
+    We must service two http requests
+    The first is the OAuth2 callback with the token in the URL fragment
+    (as specified by OAuth2 Impliciy flow standards)
+    The second has the token in the query parameters
+    so that the backend can read it
+    """
     nonce = os.urandom(8).hex()
     redirect_uri = "http://localhost:4200/sshauthz_callback"
     requrl = (authservice['authorise'] + "?response_type=token&redirect_uri=" +
-            redirect_uri + "&state="+nonce +"&client_id=" +
-            authservice['client_id'] + "&scope=" + authservice['scope'])
+              redirect_uri + "&state=" + nonce + "&client_id=" +
+              authservice['client_id'] + "&scope=" + authservice['scope'])
     webbrowser.open(requrl)
-    port=4200
+    port = 4200
     server_address = ('', port)
     handler = partial(MyRequestHandler, port, authservice['logout'])
-    httpd = HTTPServer(server_address, handler) 
+    httpd = HTTPServer(server_address, handler)
     httpd.handle_request()
     httpd.handle_request()
     path = q.get()
@@ -101,20 +140,28 @@ def do_request(authservice):
     token = params[0].split('=')[1]
     state = params[1].split('=')[1]
     if not state == nonce:
-        raise Exception('It looks like someone is playing silly buggers and intercepting messages between you and the authentication server')
+        raise Exception('OAuth2 error: A securit check failed')
     return token
 
 
 def main():
+    """
+    Read the authservers.json config file
+    Get an OAuth2 Implicit token
+    Generate an SSH key pair
+    Use the OAuth2 token to create a certificate from the pub key
+    Add the certificate to the users agent
+    """
     currpath = pathlib.Path(__file__).parent.absolute()
-    with open(os.path.join(currpath,'authservers.json'),'r') as f:
+    with open(os.path.join(currpath, 'authservers.json'), 'r') as f:
         config = json.loads(f.read())
         authservice = config[0]
 
     token = do_request(authservice)
     path = make_key()
-    sign_cert(path,token,authservice['sign'])
-    add_cert(path)
+    sign_cert(path, token, authservice['sign'])
+    add_key_agent(path)
+
 
 if __name__ == '__main__':
     main()
